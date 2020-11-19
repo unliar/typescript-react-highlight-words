7f84d0c5cc9fd5e28d86f3a93f77f860c7c63ba8
blessyou <370732889@qq.com>
2020-11-19 12:19:15 +0800

    feat: update test

--- __tests__/index.tsx
+++ __tests__/index.tsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import HighlightWords from "../src/index";
 import React from "react";
 
